ee13c802a0f1544a8384e95875ade6a440cdca4f
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-25 17:07:43 +0300

    feat: add Card component

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DUMMY_MEALS } from "../../data/dummy-meals";
+import { Card } from "../Interface/Card";
 
 import styles from "./AvailableMeals.module.css";
 
@@ -7,7 +8,9 @@ export const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
