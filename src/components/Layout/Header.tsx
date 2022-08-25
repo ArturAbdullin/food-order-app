615d1c0912ecbfee5a2efd7e2532e11349e62799
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-25 15:51:26 +0300

    feat(Header): add Cart button component

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import styles from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import { HeaderCartButton } from "./HeaderCartButton";
 
 export const Header = () => {
   return (
     <>
       <header className={styles.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="A table full of food" />
