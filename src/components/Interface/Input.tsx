db6fa7da11e70e74ba25fb54791126f5461b0ff1
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-26 17:25:52 +0300

    feat(Input): use React.forwardRef to fetch input value

--- src/components/Interface/Input.tsx
+++ src/components/Interface/Input.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 
 import styles from "./Input.module.css";
 
@@ -7,11 +7,13 @@ type InputProps = {
   input: React.InputHTMLAttributes<HTMLInputElement>;
 };
 
-export const Input: FC<InputProps> = (props) => {
-  return (
-    <div className={styles.input}>
-      <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
-    </div>
-  );
-};
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    return (
+      <div className={styles.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input} ref={ref} />
+      </div>
+    );
+  }
+);
